ce7b24019bfafec740ab17a77b5f9ce0b4bcb0d2
PseudoKnight <michael.jt.smith@gmail.com>
2018-08-26 02:44:10 -0700

    Build against 1.13.1

--- src/main/java/me/pseudoknight/chnaughty/CHNaughty.java
+++ src/main/java/me/pseudoknight/chnaughty/CHNaughty.java
@@ -9,7 +9,7 @@ import com.laytonsmith.core.extensions.MSExtension;
 public class CHNaughty extends AbstractExtension {
 
 	public Version getVersion() {
-		return new SimpleVersion(3,10,1);
+		return new SimpleVersion(3,10,2);
 	}
 
 	@Override
